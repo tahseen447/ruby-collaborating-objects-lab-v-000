23503227683056489c999668222bdce73a879c17
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-07-10 22:15:18 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -5,7 +5,7 @@ class MP3Importer
     @path = filepath
   end
 
-  def import(list_of_filenames)
+  def import(filesnames)
     list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
   end
 
