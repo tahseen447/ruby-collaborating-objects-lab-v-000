10232a67530940a20feca5d8a7281b300860778a
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-07-10 22:59:05 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,7 +11,7 @@ class Song
   def self.new_by_filename(filename)
     new_song = self.new(filename.split(" - ")[1])
     new_song.artist =  Artist.find_or_create_by_name(filename.split(" - ")[0])
-    new_song.artist.add_song(song)
+    new_song.artist.add_song(new_song)
     binding.pry
     new_song
   end
