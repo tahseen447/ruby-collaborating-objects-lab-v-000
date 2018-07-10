72bb75e655dc17b71c745c0615c2fe9411421f58
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-07-10 21:02:49 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,8 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    new_song = self.new(filename.split(" - ")[1])
+    new_song = self.new
+    new_song.name = filename.split(" - ")[1]
     new_song.artist.name = filename.split(" - ")[0]
     new_song
   end
